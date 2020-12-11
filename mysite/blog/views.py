0315d918318aeecdf8267a136c9cf734dad15ccd
KasvDollaer <zvikokasvosve@gmail.com>
2020-12-11 21:40:33 +0200

    Finished Half of the emailpart for sharing posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -25,6 +27,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
@@ -32,3 +35,25 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" \  
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']} \'s comments: {cd['comments']}"
+            send_mail(subject, message, 'zvikokasvosve@gmail.com', [cd['to']])
+            sent = True            # ... send email
+    else:
+        form = EmailPostForm()
+
+        return render(request, 'blog/post/share.html', {'post':post, 'form': form, 'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#email services setup for simple mail transfer protocol(smtp)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'zvikokasvosve@gmail.com'
+EMAIL_HOST_PASSWORD = 'kasvdollaer'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
